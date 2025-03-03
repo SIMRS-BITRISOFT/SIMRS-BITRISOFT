a53bbede79cf0f20b55533231b65ee5bcbe58bd9
paskah04 <paskah.lina.h@mail.ukrim.ac.id>
2025-03-03 21:37:56 +0700

    produk

---
+++ resources/views/content/produk-lain/aplikasi-satu-sehat.blade.php
@@ -0,0 +1,20 @@
+<section id="aplikasi-satu-sehat" class="padding-large">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="image-holder me-4 mb-4">
+                    <h3 class="display-5 fw-bold text-dark">Aplikasi Satu Sehat</h3>
+                    {{--                        <h5 class="fs-4 fw-bold text-dark">Dr. Leslie Taylor</h5>--}}
+                </div>
+            </div>
+            <div class="row col-md-8">
+                <div class="border bg-light p-4">
+                    <p>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, autem ex exercitationem facilis fuga fugiat, in maxime modi, nemo obcaecati officia perspiciatis praesentium provident quas quidem similique sit voluptates.</span>
+                        <span>Accusantium aut autem, blanditiis doloremque eius eveniet facere id inventore itaque labore nihil, nisi obcaecati omnis, quaerat reiciendis repellendus sequi voluptates? Adipisci deleniti dignissimos dolores facilis maiores, nam reprehenderit sint?</span><span>Accusantium debitis delectus dolorum ea eaque esse, laboriosam molestias nostrum obcaecati, odit omnis quam temporibus! Accusamus eos, hic ipsa ipsum maiores maxime quam sit voluptas? Dicta inventore molestias possimus repellat?</span><span>In necessitatibus quaerat vero voluptatum? Amet architecto, at atque aut consectetur consequatur consequuntur ducimus eius fugit illo inventore ipsam iure molestias nisi nobis obcaecati perferendis quia quod quos ratione tenetur!</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
